880f6119281e234babfabdd439c5c9fe90f9b5cb
Lugertr <nikitavishnev@bk.ru>
2022-11-30 14:35:36 +0300

    fixTokken

--- src/app/services/host-getter.service.ts
+++ src/app/services/host-getter.service.ts
@@ -1,12 +1,12 @@
 import { DOCUMENT } from '@angular/common';
 import { inject, InjectionToken } from '@angular/core';
 
-export const HOSTGETTER = new InjectionToken<string>('Window object', {
+export const HOSTGETTER = new InjectionToken<Window>('Window object', {
   factory: () => {
     const { defaultView } = inject(DOCUMENT);
     if (!defaultView) {
       throw new Error('Window is not available');
     }
-    return defaultView.URL.toString() + Math.random();
+    return defaultView;
   },
 });
